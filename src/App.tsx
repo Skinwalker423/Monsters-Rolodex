49f23df943fb746a1222262559df57bb5d7b08cc
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-17 22:40:48 -0700

    TS to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import React, {useState, useEffect, ChangeEvent} from "react";
+import axios from 'axios';
+import CreateMonsterList from "./components/card-list/createMonsterList";
+import SearchBar from "./components/search-box/SearchBar";
+import { getData } from "./utils/data.utils";
+
+
+type Monsters = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+
+const App = () => {
+
+  const [value, setValue] = useState('');
+  const [monsters, setMonsters] = useState<Monsters[]>([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [title, setTitle] = useState('Monsters Rolodex');
+
+
+  
+
+  useEffect(()=> {
+  //   console.log('useeffect1');
+  //   const getMonsters = async() => {
+  //       const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+  //     setMonsters(res.data);
+  // }
+  //   getMonsters();
+
+    const fetchUsers = async () => {
+      const users = await getData<Monsters[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    }
+    
+    fetchUsers();
+
+  }, [])
+
+  useEffect(()=> {
+    console.log('filtered mosters');
+    const newFilteredMonsters = monsters.filter(monster => {
+          return monster.name.toLowerCase().includes(value)
+      });
+    setFilteredMonsters(newFilteredMonsters);
+      
+  }, [value, monsters])
+
+  const onValueChange = (e: ChangeEvent<HTMLInputElement>) : void => {
+    const inputValue = e.target.value.toLowerCase();
+    setValue(inputValue);
+    setTitle(inputValue);
+
+  }
+
+
+
+  
+
+    return(
+      <div className="ui container">
+        <h1 className="app-title">{title}</h1>
+        <SearchBar 
+          onChangeHandler={onValueChange} 
+          placeholder='search monster'
+          className='search-box'
+        />
+        <CreateMonsterList monsters={filteredMonsters} />
+      </div>
+    )
+  
+}
+
+export default App;
+
+
+
+
+
+
